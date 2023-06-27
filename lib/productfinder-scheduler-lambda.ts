ffc64a8d9f25c34d8cada00c0cfc88e707e4e77f
Yan Zhou <Yan.Zhou2@nike.com>
2023-06-27 14:43:23 +0800

    export sqs arn and change the import type of role

--- lib/productfinder-scheduler-lambda.ts
+++ lib/productfinder-scheduler-lambda.ts
@@ -1,6 +1,7 @@
 import {Construct} from "constructs";
 import {Code, Function, Runtime} from "aws-cdk-lib/aws-lambda";
 import {Role} from "aws-cdk-lib/aws-iam";
+import {Fn} from "aws-cdk-lib";
 
 export class ProductFinderSchedulerLambda extends Construct {
 
@@ -11,13 +12,14 @@ export class ProductFinderSchedulerLambda extends Construct {
                 allowUserTableName : string,
                 topicArn : string) {
         super(scope, id);
-
+        //role arn
+        const productFinderRoleArn = Fn.importValue('ProductFinderRoleArn');
         this.lambda = new Function(this, 'launchProductFinderSchedulerLambda', {
             functionName : 'launch-productfinder-scheduler-lambda',
             runtime : Runtime.NODEJS_16_X,
             code: Code.fromAsset('src/create-entry/lib'),
             handler: 'index.handler',
-            role : Role.fromRoleName(this, 'existingSchedulerLambdaRole', 'launch-productfinder-role'),
+            role : Role.fromRoleArn(this, 'existingSchedulerLambdaRole', productFinderRoleArn),
             description: 'Initialize a productFinder automation request',
             environment: {
                 ALLOWEDUSERS_TABLE_NAME: allowUserTableName,
--- lib/productfinder-scheduler-sqs.ts
+++ lib/productfinder-scheduler-sqs.ts
@@ -1,6 +1,8 @@
 import * as sqs from 'aws-cdk-lib/aws-sqs';
 import { Construct } from 'constructs';
 import {Queue} from "aws-cdk-lib/aws-sqs";
+import * as cdk from "aws-cdk-lib";
+import {CfnOutput} from "aws-cdk-lib";
 
 export class ProductFinderSchedulerSqs extends Construct {
   public readonly queue: sqs.Queue;
@@ -10,6 +12,11 @@ export class ProductFinderSchedulerSqs extends Construct {
     //create sqs
     this.queue = new Queue(this, 'launchProductFinderSchedulerQueue', {
       queueName : 'launch-productfinder-scheduler-queue'
+    });
+    new CfnOutput(this, 'ProductFinderSchedulerSQSArn', {
+      value: this.queue.queueArn,
+      description: 'The arn name of scheduler sqs queue',
+      exportName: 'ProductFinderSchedulerSQSArn',
     });
   }
 }
